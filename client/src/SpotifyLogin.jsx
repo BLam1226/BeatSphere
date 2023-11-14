2040145484235b0bab567c3eeae66b4562e46fee
BLam1226 <brendanlam0@gmail.com>
2023-11-14 16:25:23 -0500

    more fixes

--- client/src/SpotifyLogin.jsx
+++ client/src/SpotifyLogin.jsx
@@ -2,7 +2,7 @@ import React from 'react';
 
 import { Container } from 'react-bootstrap';
 
-const AUTH_URL = "https://accounts.spotify.com/authorize?client_id=be06c16b9fca42da816a259817bad5b3&response_type=code&redirect_uri=https://localhost:3000/Player&scope=streaming%20user-read-email%20user-read-private%20user-library-read%20user-library-modify%20user-read-playback-state%20user-modify-playback-state"
+const AUTH_URL = "https://accounts.spotify.com/authorize?client_id=be06c16b9fca42da816a259817bad5b3&response_type=code&redirect_uri=https://powerful-earth-51293-6f18607437c5.herokuapp.com/Player&scope=streaming%20user-read-email%20user-read-private%20user-library-read%20user-library-modify%20user-read-playback-state%20user-modify-playback-state"
 
 export default function SpotifyLogin() {
     return (
