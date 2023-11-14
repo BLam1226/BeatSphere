2ebf0e6574ac678c51c7768bfb620fa729a6ce2d
BLam1226 <brendanlam0@gmail.com>
2023-11-14 15:52:50 -0500

    testing

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -8,6 +8,9 @@ import Login from "./pages/Login";
 import Signup from "./pages/Signup";
 import fetchTopTracks from "./lastFM";
 import Player from "./pages/Player";
+import Dashboard from "./Dashboard";
+
+const code = new URLSearchParams(window.location.search).get("code");
 
 function App() {
   const [country, setCountry] = useState("");
@@ -25,27 +28,28 @@ function App() {
     setCountry(val);
   }
 
-  return (
-    <>
-    <Navbar />
-       <div className="container">
-         <Routes>
-           <Route exact className="loginroute" path="/Login" element={<Login />} />
-           <Route exact className="signuproute" path="/Signup" element={<Signup />} />
-           <Route path="/Player" element={<Player />} />
-         </Routes>
-        </div>
-      <h1 className='country'>{country}</h1>
-      <div className='dropdown'>
-        <CountryDropdown value={country} onChange={(val) => selectCountry(val)} />
-      </div>
-      <div className="card-container">
-        {topTracks.map((track, index) => (
-          <Card key={index} track={track} artist={track.artist.name} />
-        ))}
-      </div>
-    </>
-  );
+  return code ? <Dashboard code={code} /> : <Login />;
+  // return (
+  //   <>
+  //   <Navbar />
+  //      <div className="container">
+  //        <Routes>
+  //          <Route exact className="loginroute" path="/Login" element={<Login />} />
+  //          <Route exact className="signuproute" path="/Signup" element={<Signup />} />
+  //          <Route path="/Player" element={<Player />} />
+  //        </Routes>
+  //       </div>
+  //     <h1 className='country'>{country}</h1>
+  //     <div className='dropdown'>
+  //       <CountryDropdown value={country} onChange={(val) => selectCountry(val)} />
+  //     </div>
+  //     <div className="card-container">
+  //       {topTracks.map((track, index) => (
+  //         <Card key={index} track={track} artist={track.artist.name} />
+  //       ))}
+  //     </div>
+  //   </>
+  // );
 }
 
 export default App;
