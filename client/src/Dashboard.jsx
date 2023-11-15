7b37cfa375ccc078061d72606b4b6a14b5d1a967
BLam1226 <brendanlam0@gmail.com>
2023-11-15 17:01:59 -0500

    some styling

--- client/src/Dashboard.jsx
+++ client/src/Dashboard.jsx
@@ -5,10 +5,11 @@ import SpotifyWebApi from 'spotify-web-api-node';
 import TrackSearchResult from './TrackSearchResult';
 import SpotifyPlayerApp from './SpotifyPlayer';
 import axios from 'axios';
+import './Dashboard.css'; 
 
 const spotifyApi = new SpotifyWebApi({
     clientId: "be06c16b9fca42da816a259817bad5b3"
-})
+});
 
 export default function Dashboard({ code }) {
     const accessToken = useAuth(code);
@@ -24,7 +25,7 @@ export default function Dashboard({ code }) {
     }
 
     useEffect(() => {
-        if (!playingTrack) return
+        if (!playingTrack) return;
 
         axios.get('https://beat-sphere-be92cb90b28f.herokuapp.com/lyrics', {
             params: {
@@ -32,58 +33,65 @@ export default function Dashboard({ code }) {
                 artist: playingTrack.artist
             }
         }).then(res => {
-            setLyrics(res.data.lyrics)
-        })
-    }, [playingTrack])
+            setLyrics(res.data.lyrics);
+        });
+    }, [playingTrack]);
 
     useEffect(() => {
-        if (!accessToken) return
+        if (!accessToken) return;
         spotifyApi.setAccessToken(accessToken);
-    }, [accessToken])
+    }, [accessToken]);
 
     useEffect(() => {
-        if (!search) return setSearchResults([])
-        if (!accessToken) return
+        if (!search) return setSearchResults([]);
+        if (!accessToken) return;
 
-        let cancel = false
+        let cancel = false;
         spotifyApi.searchTracks(search).then(res => {
-            if (cancel) return
+            if (cancel) return;
             setSearchResults(
                 res.body.tracks.items.map(track => {
                     const smallestAlbumImage = track.album.images.reduce(
                         (smallest, image) => {
-                            if (image.height < smallest.height) return image
-                            return smallest
+                            if (image.height < smallest.height) return image;
+                            return smallest;
                         },
                         track.album.images[0]
-                    )
+                    );
 
                     return {
                         artist: track.artists[0].name,
                         title: track.name,
                         uri: track.uri,
                         albumUrl: smallestAlbumImage.url
-                    }
+                    };
                 })
-            )
-        })
+            );
+        });
 
-        return () => (cancel = true)
-    }, [search, accessToken])
+        return () => (cancel = true);
+    }, [search, accessToken]);
 
-    return <Container className="d-flex flex-column py-2" style={{ height: "100vh" }}>
-        <Form.Control type="search" placeholder="Search Songs/
-        Artists" value={search} onChange={e => setSearch(e.target.value)} />
-        <div className="flex-grow-1 my-2" style={{ overflowY: "auto" }}>
-            {searchResults.map(track => (
-                <TrackSearchResult track={track} key={track.uri} chooseTrack={chooseTrack}/>
-            ))}
-            {searchResults.length === 0 && (
-                <div className="text-center" style={{ whiteSpace: "pre" }}>
-                    {lyrics}
-                </div>
-            )}
-        </div>
-        <div><SpotifyPlayerApp accessToken={accessToken} trackUri={playingTrack?.uri} /></div>
-    </Container>
+    return (
+        <Container className="dashboard-container">
+            <Form.Control
+                className="search-input"
+                type="search"
+                placeholder="Search Songs/Artists"
+                value={search}
+                onChange={e => setSearch(e.target.value)}
+            />
+            <div className="results-container">
+                {searchResults.map(track => (
+                    <TrackSearchResult track={track} key={track.uri} chooseTrack={chooseTrack} />
+                ))}
+                {searchResults.length === 0 && (
+                    <div className="text-center">{lyrics}</div>
+                )}
+            </div>
+            <div>
+                <SpotifyPlayerApp accessToken={accessToken} trackUri={playingTrack?.uri} />
+            </div>
+        </Container>
+    );
 }
